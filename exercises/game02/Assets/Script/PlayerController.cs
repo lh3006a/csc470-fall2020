d31c4dd209cc2dad54992895a35b35b17e1f5ff9
lh3006a <lh3006a@student.american.edu>
2020-09-18 14:20:35 -0400

    exported project

--- exercises/game02/Assets/Script/PlayerController.cs
+++ exercises/game02/Assets/Script/PlayerController.cs
@@ -10,11 +10,6 @@ public class PlayerController : MonoBehaviour
 	public Text scoreText;
 	GameObject dinosaur;
 
-	//rb = GetComponent<Rigidbody>();
-	/**private float movementX;
-	private float movementY;
-	private float movementZ;
-	**/
     // Start is called before the first frame update
     void Start()
     {
@@ -24,28 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	//transform.rotation = new Vector3(-2.955f, -2.109f, -3.613f);
-    	/**if(rotation.x != -2.995){
-    		transform.rotation.x = -2.955;
-    	}
-    	if(rotation.y != -2.109){
-    		transform.rotation.y = -2.109;
-    	}
-    	if(rotation.z != -3.613){
-    		transform.rotation.z = -3.613;
-    	}
-    	**/
-    	//transform.rotation(-2.955f, -2.109f, -3.613f);
-    	/**if (Input.GetKeyDown("space")){
-    		transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-    		//transform.Translate(transform.forward * Time.deltaTime * speed);
-    		transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-    		transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-    		transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
-    		
-    		Debug.Log("space");   
-    	}
-		**/
+    	
     	if(Input.GetKey("left")){
     		transform.Translate((transform.forward* -1) * Time.deltaTime * speed); 
     	}
@@ -77,27 +51,13 @@ public class PlayerController : MonoBehaviour
     			
     		}
     			//score = score + 1;
-    			Destroy(other.gameObject);
+    		Destroy(other.gameObject);
     			
-    			scoreText.text = score.ToString();
+    		scoreText.text = score.ToString();
 
 
     		
     	}
     }
-    
-    /**void OnMove(InputValue movementValue)
-    {
 
-    	Vector3 movementVector = movementValue.Get<Vector3>();
-    	movementX = movementVector.x;
-    	movementY = movementVector.y;
-    	movementZ = movementVector.z;
-
-    }
-    void FixedUpdate()
-    {
-    	rb.AddForce(movement * 5);
-    }
-    **/
 }
