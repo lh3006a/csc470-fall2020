9c59194f45e03d655b112610e0ac344655cc283c
lh3006a <lh3006a@student.american.edu>
2020-10-01 13:56:41 -0400

    game 3 done

---
+++ exercises/game03/Assets/Scripts/CellScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CellScript : MonoBehaviour
+{
+
+	public Renderer rend;
+	public Color aliveColor;
+	public Color deadColor;
+	public float aliveHeight = .02f;
+	public float deadHeight = .01f;
+
+	public int x = -1;
+	public int y = -1;
+	public Text aliveScoreText;
+	public Text deadScoreText;
+
+
+	public bool nextAlive;
+	private bool _alive = false;
+    public bool Alive
+   	{
+        get
+        {
+            return this._alive;
+        }
+        set
+        {
+            this._alive = value;
+            
+            if (this._alive) {
+				rend.material.color = aliveColor;
+				transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + 1, transform.localScale.z);
+
+			} else {
+				rend.material.color = deadColor;
+				if(transform.localScale.y >1){
+					transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - 1, transform.localScale.z);
+				}
+			}
+		}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+		this.Alive = Random.value < 0.5f; 
+		GameObject aliveObj = GameObject.Find("AliveScore");
+		aliveScoreText = aliveObj.GetComponent<Text>();
+		GameObject deadObj = GameObject.Find("DeadScore");
+		deadScoreText = deadObj.GetComponent<Text>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+   	{
+   	   	if(other.gameObject.CompareTag("Ball"))
+   	   	{
+   	   		if(this.Alive){
+   	   			other.GetComponent<Renderer>().material.color = new Color (Random.value, Random.value, Random.value);
+   	   			GameManagerScript.aliveScore++;
+   	   			aliveScoreText.text = "Alive Score: " + GameManagerScript.aliveScore.ToString();
+   	   		}
+   	   		else if(!this.Alive){
+   	   			other.GetComponent<Renderer>().material.color = new Color (0,0,0);
+   	   			GameManagerScript.deadScore++;
+   	   			deadScoreText.text = "Dead Score: " + GameManagerScript.deadScore.ToString();
+
+
+   	   		}
+   	   	}
+
+   }
+
+}
---
+++ exercises/game03/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+	public GameObject cellPrefab;
+	public GameObject ballPrefab;
+	GameObject ground;
+	public CellScript[,] grid;
+
+	int gridWidth = 100;
+	int gridHeight = 100;
+	float cellDimension = 0.5f;
+	float padding = 0.1f;
+	public static int aliveScore;
+	public static int deadScore;
+
+	int time = 0;
+	float timer = 0;
+	float timerRate = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	grid = new CellScript[gridWidth, gridHeight];
+    	for (int x = 0; x < gridWidth; x++){
+    		for(int y =0; y < gridHeight; y++){
+    			Vector3 pos = new Vector3(x *(cellDimension + padding), 0, y *(cellDimension + padding));
+    			GameObject cellOb = Instantiate(cellPrefab, pos, Quaternion.identity);
+    			cellOb.transform.localScale = new Vector3(cellDimension, cellDimension, cellDimension);
+    			CellScript cs = cellOb.GetComponent<CellScript>();
+    			cs.x = x;
+    			cs.y = y;
+    			grid[x,y] = cs;
+    		}
+    	}
+    	ground = GameObject.Find("Ground");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	timer -= Time.deltaTime;
+    	if(timer < 0){
+    		generateNextState();
+    		timer = timerRate;
+    	}
+        
+    }
+    void generateNextState()
+    {
+    	time++;
+    	for(int x = 0; x < gridWidth; x++){
+    		for(int y = 0; y < gridHeight; y++){
+    			List<CellScript> liveNeighbors = gatherLiveNeighbors(x,y);
+    			if(grid[x,y].Alive && liveNeighbors.Count <2){
+    				grid[x,y].nextAlive = false;
+    			}
+    			else if (grid[x,y].Alive && (liveNeighbors.Count == 2 || liveNeighbors.Count == 3)){
+    				grid[x,y].nextAlive = true;
+    			}
+    			else if (grid[x,y].Alive && liveNeighbors.Count > 3){
+    				grid[x,y].nextAlive = false;
+    			}
+    			else if (!grid[x,y].Alive && liveNeighbors.Count == 3){
+    				grid[x,y].nextAlive = true;
+    			}
+    		}
+
+    	}
+    	for(int x = 0; x < gridWidth; x++){
+    		for(int y = 0; y < gridHeight; y++){
+    			grid[x,y].Alive = grid[x,y].nextAlive;
+    		}
+    	}
+    }
+    List<CellScript> gatherLiveNeighbors(int x, int y)
+    {
+    	List<CellScript> liveNeighbors = new List<CellScript>();
+    	for(int i = Mathf.Max(0, x-1); i <= Mathf.Min(gridWidth - 1, x + 1); i++){
+    		for(int j = Mathf.Max(0, y-1); j <= Mathf.Min(gridHeight - 1, y + 1); j++){
+    			if(!(x == i && y == j)){
+    				if(grid[i,j].Alive){
+    					liveNeighbors.Add(grid[i,j]);
+    				}
+    			}
+    		}
+    	}
+    	return liveNeighbors;
+    }
+    public void BallDrop()
+    {
+    	Debug.Log("click");
+    	for(int i = 0; i<=7; i++){
+    		Vector3 ballPos = new Vector3(ground.transform.position.x + Random.Range(-30,30),
+    		ground.transform.position.y + 30f, ground.transform.position.z+ Random.Range(-30,30));
+    		Instantiate(ballPrefab, ballPos, Quaternion.identity);
+    	}
+    	
+    }
+    
+}
